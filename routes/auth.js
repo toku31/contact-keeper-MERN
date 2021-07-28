45eb39b9925ece9041d94b84a7219fcc3d4e6e8e
garden627 <garden627@yahoo.co.jp>
2021-07-28 22:47:22 +0900

    Section7 Backend

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,74 @@
 const express = require('express')
 const router = express.Router();
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../middleware/auth')
+
+const User = require('../models/User');
+// ログインユーザを取得する
 // @route    GET api/auth
 // @desc     Get looged in user
 // @access   Private
-router.get('/', (req, res) => {   // ここの'/'は'api/auth'
-  res.send('Get looged in user')
+router.get('/', auth, async (req, res) => {   // ここの'/'は'api/auth'
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  }catch (err){
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
 });
 
 // @route    POST api/auth
 // @desc     Auth user & get token
 // @access   Public
-router.post('/', (req, res) => {   // ここの'/'は'api/auth'
-  res.send('Log in user')
+router.post(
+  '/',
+  [check('email', 'Please include a valid email').isEmail(),
+  check('password',`Password is required`).exists()
+  ],
+async(req, res) => {   // ここの'/'は'api/auth'
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email })
+
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials'})
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if(!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials'})
+      }
+
+      const  payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload, 
+        config.get('jwtSecret'), 
+        {
+          expiresIn: 360000
+        }, (err, token) => {
+          if(err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.err(err.message);
+      res.status(500).send('Server Error')
+    }
 });
 
 module.exports = router;
